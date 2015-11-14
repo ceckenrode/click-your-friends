ae6a50f3d1f569d6790e1ca4e7156248833e4bf4
ceckenrode <chriseckenrode@me.com>
2015-11-14 02:50:12 -0500

    cleaned up code

--- js/clickFriend.js
+++ js/clickFriend.js
@@ -1,41 +1,36 @@
-//var started = false;
-
+var started = false;
 var elem = document.getElementById("start");
 
 
-function change()
-{
+function change() {
   started = true;
-  
-  var timer = setInterval(function(){countDown()}, 1000);
+
+  var timer = setInterval(function() {
+    countDown()
+  }, 1000);
   setTimeout(function() {clearInterval(timer);}, 21000);
-  setTimeout(function(){restart()}, 21000);
+  setTimeout(function() {restart();}, 21000);
 }
 
 function countDown() {
- if (started === true) {
-  elem.onclick = "#" ;
-}
+  if (started === true) {
+    elem.onclick = "#";
+  }
   if (elem.text === "Start Timer" || elem.text === "Play Again?") {
-  elem.style.color = "#02E148";
-  elem.text = "20"; 
-} else if (elem.text > 0) {
-  elem.text--;
-  if (elem.text < 15 && elem.text > 10){
-    elem.style.color = "#E56414";
+    elem.style.color = "#02E148";
+    elem.text = "20";
+  } else if (elem.text > 0) {
+    elem.text--;
+    if (elem.text < 15 && elem.text > 10) {
+      elem.style.color = "#E56414";
+    } else if (elem.text < 6) {
+      elem.style.color = "red";
+    }
   }
-  else if (elem.text < 6) {
-    elem.style.color = "red";
-  }
-} 
 }
 
 function restart() {
   elem.text = "Play Again?";
-  elem.onclick = function(){change()};
-  //started = false;
+  started = false;
+  elem.onclick = function() {change(); };
 }
-
-//function nothing () {
-
-//}
